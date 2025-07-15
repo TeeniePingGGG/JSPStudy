af74d5f3029f2694d252f67ce91c1372ef1d1e7e
jinkyeong <lucyvery@naver.com>
2025-07-15 19:30:46 +0900

    7/15

--- src/main/java/common/JDBConnect.java
+++ src/main/java/common/JDBConnect.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import jakarta.servlet.ServletContext;
+
 		
 public class JDBConnect {
 	public Connection con;
@@ -16,7 +18,7 @@ public class JDBConnect {
 	//기본 생성자	
 	public JDBConnect() {
 		try {
-			// 오라클 드라이버 로드
+			// 오라클 드라이버 로드	
 			Class.forName("oracle.jdbc.OracleDriver");
 			//커넥션 URL, 아이디, 비번							
 			String url = "jdbc:oracle:thin:@localhost:1521:xe";
@@ -31,6 +33,35 @@ public class JDBConnect {
 			e.printStackTrace();
 		}
 	}
+		// 인수생성자: 4개의 매개변수 선언
+	   public JDBConnect(String driver, String url, String id, String pwd) {
+		      try {
+		         Class.forName("oracle.jdbc.OracleDriver");
+		         con = DriverManager.getConnection(url, id, pwd);
+		         System.out.println("DB 연결 성공(인수 생성자1)");
+		      }
+		      catch (Exception e) {
+		         e.printStackTrace();
+		      }
+		   }
+	   
+	   public JDBConnect(ServletContext application) {
+			try {
+				String driver = application.getInitParameter("OracleDriver");
+				Class.forName(driver);
+				//커넥션 URL, 아이디, 비번							
+				String url = application.getInitParameter("OracleURL");
+				String id = application.getInitParameter("OracleId");
+				String pwd = application.getInitParameter("OraclePwd");
+				con = DriverManager.getConnection(url,id,pwd);
+				
+				System.out.println("DB연결 성공(인수 생성자2)");
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+
 	
 	// 자원 반납 하기
 	public void close() {
